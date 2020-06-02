1f85e1b663294418519d548ed4c61247c7700428
Bruno Baptista <bruno.baptistapt1999@gmail.com>
2020-06-02 10:45:00 +0100

    Added and implement load and save

--- app/app/src/main/java/pt/isel/poo/covid/MainActivity.java
+++ app/app/src/main/java/pt/isel/poo/covid/MainActivity.java
@@ -4,8 +4,13 @@ package pt.isel.poo.covid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 import pt.isel.poo.covid.model.Level;
@@ -16,12 +21,12 @@ import pt.isel.poo.covid.view.LevelView;
 public class MainActivity extends AppCompatActivity {
 
     private final String FILENAME = "covid_levels.txt";
+    private final String SAVEFILE = "saved_level.txt";
     private Level level ;
     private Scanner in  ;
-
-
-
-
+    private Button saveButton,loadButton;
+    private int currentLevel = 1;
+    private int savedLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +49,30 @@ public class MainActivity extends AppCompatActivity {
         }
         new LevelView(panel,level);
 
+        findViewById(R.id.saveButton).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try (PrintStream output = new PrintStream(openFileOutput(SAVEFILE, MODE_PRIVATE))) {
+                    savedLevel = currentLevel;
+                    level.save(output,savedLevel);
+                    System.out.println(MainActivity.this.getFilesDir().getAbsolutePath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
 
+        findViewById(R.id.loadButton).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    in = new Scanner(openFileInput(SAVEFILE));
+
+                } catch (FileNotFoundException | Loader.LevelFormatException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
     }
 }
