e210f78e9255a55d344d59521e60f2d42056fda4
Bruno Baptista <bruno.baptistapt1999@gmail.com>
2020-06-07 18:27:08 +0100

    Add hero movement logic.

--- app/app/src/main/java/pt/isel/poo/covid/model/Hero.java
+++ app/app/src/main/java/pt/isel/poo/covid/model/Hero.java
@@ -5,11 +5,63 @@ package pt.isel.poo.covid.model;
  */
 public class Hero extends LevelElement {
 
+    private int arenaWidth,arenaHeight;
+    private Direction currentDirection;
+    private boolean isDead;
+    private Level level;
+
     /**
-     * Initiates a wall placing it at the given location.
+     * Initiates a Hero placing it at the given location.
      * @param location  the location where the wall is placed.
      */
-    public Hero(Location location) {
+    public Hero(Location location,Direction initialDirection,int arenaWidth,int arenaHeight,Level level) {
         super(location);
+        this.currentDirection = initialDirection;
+        this.arenaHeight = arenaHeight;
+        this.arenaWidth = arenaWidth;
+        this.isDead = false;
+        this.level = level;
+
+
+
     }
+
+
+    public boolean canMove(Direction direction){
+        final Location newLocation = position.add(direction);
+        return newLocation.x >= 0 && newLocation.x < arenaWidth &&
+                newLocation.y >= 0 && newLocation.y < arenaHeight &&
+                !(level.getElementAt(newLocation.x,newLocation.y) instanceof Wall) &&
+                !(level.getElementAt(newLocation.x,newLocation.y) instanceof TrashCan)&&
+                !(level.getElementAt(newLocation.x,newLocation.y) instanceof Virus);
+
+    }
+
+    /**
+     * Moves the hero in the selected direction
+     */
+    public void move() {
+       /* if (isDead)
+            throw new IllegalStateException();*/
+        if (canMove(currentDirection)) {
+
+            Location oldPosition = position;
+            position = position.add(currentDirection);
+            level.swap(oldPosition,position);
+
+        }
+    }
+
+    public boolean isDead () {
+        return isDead;
+    }
+
+    public Direction getHeroDirection (){
+        return currentDirection;
+
+    }
+    public void changeDirection(Direction newDirection) {
+        currentDirection = newDirection;
+    }
+
 }
