541a354c6098e96e8ddd45238043fc52967bb1c6
Bruno Baptista <bruno.baptistapt1999@gmail.com>
2020-06-12 11:52:34 +0100

    Add on change method, to update the position of the elements (change afterwards to a more efficient way)

--- app/app/src/main/java/pt/isel/poo/covid/view/LevelView.java
+++ app/app/src/main/java/pt/isel/poo/covid/view/LevelView.java
@@ -1,11 +1,17 @@
 package pt.isel.poo.covid.view;
+
+import android.content.Context;
+
 import pt.isel.poo.covid.model.Hero;
+
+
 import pt.isel.poo.covid.model.Level;
 import pt.isel.poo.covid.model.LevelElement;
 import pt.isel.poo.covid.model.Location;
 import pt.isel.poo.covid.model.TrashCan;
 import pt.isel.poo.covid.model.Virus;
 import pt.isel.poo.covid.model.Wall;
+
 import pt.isel.poo.covid.tile.Tile;
 import pt.isel.poo.covid.tile.TilePanel;
 
@@ -13,11 +19,27 @@ public class LevelView {
 
     private final TilePanel panel;
     private final Level level;
+    private Context context;
 
-    public LevelView(TilePanel panel, Level level) {
+
+
+    public LevelView(TilePanel panel, Level level,Context context) {
         this.panel = panel;
         this.level = level;
+        this.context = context;
         initialize();
+        
+    }
+
+    
+    public void onChange(Level level){
+        for(int x = 0; x < level.arenaWidth; ++x) {
+            for(int y = 0; y < level.arenaHeight; ++y) {
+                // TODO: Prevent the instantiation of all these objects
+                updatePosition(new Location(x, y));
+
+            }
+        }
 
     }
 
@@ -35,19 +57,17 @@ public class LevelView {
         final LevelElement element = level.getElementAt(position.x, position.y);
         Tile tile = null;
 
-        //System.out.println("X:" + position.x + " Y:" + position.y + " " + " Element:" + element);
-
         if (element instanceof Hero)
-            tile = new HeroTile();
+            tile = new HeroTile(context);
 
         if (element instanceof TrashCan)
-            tile = new TrashCanTile();
+            tile = new TrashCanTile(context);
 
         if  (element instanceof Wall)
-            tile = new WallTile();
+            tile = new WallTile(context);
 
         if  (element instanceof Virus)
-            tile = new VirusTile();
+            tile = new VirusTile(context);
 
         panel.setTile(position.x, position.y, tile);
     }
