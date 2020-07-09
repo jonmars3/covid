0f09d24d0084bbabcb7d9fc49c6e04a7e6a63f1d
Bruno Baptista <bruno.baptistapt1999@gmail.com>
2020-07-09 19:02:57 +0100

    Created folder for the element factory.

--- app/app/src/main/java/pt/isel/poo/covid/ElementFactory/ElementFactory.java
+++ app/app/src/main/java/pt/isel/poo/covid/ElementFactory/ElementFactory.java
@@ -1,7 +1,16 @@
-package pt.isel.poo.covid.model;
+package pt.isel.poo.covid.ElementFactory;
+
+import pt.isel.poo.covid.model.Hero;
+import pt.isel.poo.covid.model.Level;
+import pt.isel.poo.covid.model.LevelElement;
+import pt.isel.poo.covid.model.Location;
+import pt.isel.poo.covid.model.TrashCan;
+import pt.isel.poo.covid.model.Virus;
+import pt.isel.poo.covid.model.Wall;
+
 public class ElementFactory {
 
-    public LevelElement getElement(int c,int l,char type,Level level){
+    public LevelElement getElement(int c, int l, char type, Level level){
         switch (type) {
             case '@':
 
